1391a715da1b59bc7ee1bc4818bac7cd7344ff9d
elias-nasser <58058223+elias-nasser@users.noreply.github.com>
2024-01-02 01:19:54 +0200

    when click on darkmode button the color is changed

--- lib/navigationDrawer/navigation_drawer.dart
+++ lib/navigationDrawer/navigation_drawer.dart
@@ -10,6 +10,8 @@ import '../auth/sign_in.dart';
 import '../auth/sign_up.dart';
 
 const String _baseURL = 'https://nasserhotel.000webhostapp.com/';
+Random random = Random();
+int? r ,g,b;
 
 class navigationDrawer extends StatefulWidget {
   const navigationDrawer({Key? key}) : super(key: key);
@@ -26,77 +28,94 @@ class _navigationDrawerState extends State<navigationDrawer> {
     String? fName = authProvider.firstName;
     String? email = authProvider.email;
     String? profileImage = authProvider.profileImage;
-    Random random = Random();
     return Drawer(
       child: Theme(
-        data: ThemeData(
-          brightness: authProvider.isDarkMode ? Brightness.dark : Brightness.light,
-          canvasColor: authProvider.isDarkMode ? Colors.grey[850] : Colors.white,
-        ),
-        child: Container(
-          color: authProvider.isDarkMode ? Colors.grey[850] : Colors.white,
-          child: Column(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: isLoggedIn ? Text(fName!) : null,
-              accountEmail: isLoggedIn ? Text(email!) : null,
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  '$_baseURL/assets/picture/profiles/$profileImage',
+          data: ThemeData(
+            brightness:
+                authProvider.isDarkMode ? Brightness.dark : Brightness.light,
+            canvasColor:
+                authProvider.isDarkMode ? Colors.grey[850] : Colors.white,
+          ),
+          child: Container(
+            color: authProvider.isDarkMode ? Colors.grey[850] : Colors.white,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  child: UserAccountsDrawerHeader(
+                    accountName: isLoggedIn ? Text(fName!) : null,
+                    accountEmail: isLoggedIn ? Text(email!) : null,
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        '$_baseURL/assets/picture/profiles/$profileImage',
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: authProvider.isDarkMode
+                          ? Colors.grey[650]
+                          : Color.fromRGBO(
+                        r= random.nextInt(256),
+                        g= random.nextInt(256),
+                        b= random.nextInt(256),
+                        1.0,
+                      ),
+                    ),
+                  ),
+                ),                ListTile(
+                  title: Text('Book Room'),
+                  leading: Icon(Icons.meeting_room),
+                  onTap: () => navigateToPage(context, ShowRooms()),
                 ),
-              ),
-              decoration: BoxDecoration(
-                color: authProvider.isDarkMode ? Colors.grey[650] : Color.fromRGBO(random.nextInt(256), random.nextInt(256), random.nextInt(256), 1.0),
-              ),
+                ListTile(
+                  title: Text('Book Event'),
+                  leading: Icon(Icons.event),
+                  onTap: () => navigateToPage(context, ShowEvents()),
+                ),
+                if (isLoggedIn)
+                  ListTile(
+                    title: Text('Dashboard'),
+                    leading: Icon(Icons.dashboard),
+                    onTap: () => navigateToPage(context, DashboardPage()),
+                  ),
+                if (isLoggedIn)
+                  ListTile(
+                    title: Text('Logout'),
+                    leading: Icon(Icons.exit_to_app),
+                    onTap: () {
+                      authProvider.logout();
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => SignInPage()),
+                      );
+                    },
+                  ),
+                if (!isLoggedIn)
+                  ListTile(
+                    title: Text('Sign In'),
+                    onTap: () => navigateToPage(context, SignInPage()),
+                  ),
+                if (!isLoggedIn)
+                  ListTile(
+                    title: Text('Sign Up'),
+                    onTap: () => navigateToPage(context, SignUpPage()),
+                  ),
+                Spacer(),
+                ListTile(
+                  title: Text('Dark Mode'),
+                  trailing: Switch(
+                    activeColor: r != null ? Color.fromRGBO(r!,
+                        g!, b!, 1.0) : Colors.green,
+                    value: authProvider.isDarkMode,
+                    onChanged: (value) {
+                      setState(() {
+                        authProvider.toggleDarkMode(value);
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text('Book Room'),
-              onTap: () => navigateToPage(context, ShowRooms()),
-            ),
-            ListTile(
-              title: Text('Book Event'),
-              onTap: () => navigateToPage(context, ShowEvents()),
-            ),
-            if (isLoggedIn)
-              ListTile(
-                title: Text('Dashboard'),
-                onTap: () => navigateToPage(context, DashboardPage()),
-              ),
-            if (isLoggedIn)
-              ListTile(
-                title: Text('Logout'),
-                onTap: () {
-                  authProvider.logout();
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => SignInPage()),
-                  );
-                },
-              ),
-            if (!isLoggedIn)
-              ListTile(
-                title: Text('Sign In'),
-                onTap: () => navigateToPage(context, SignInPage()),
-              ),
-            if (!isLoggedIn)
-              ListTile(
-                title: Text('Sign Up'),
-                onTap: () => navigateToPage(context, SignUpPage()),
-              ),
-            Spacer(),
-            ListTile(
-              title: Text('Dark Mode'),
-              trailing: Switch(
-                value: authProvider.isDarkMode,
-                onChanged: (value) {
-                  setState(() {
-                    authProvider.toggleDarkMode(value);
-                  });
-                },
-              ),
-            ),
-          ],
-        ),)
-      ),
+          )),
     );
   }
 
@@ -108,7 +127,7 @@ class _navigationDrawerState extends State<navigationDrawer> {
         const end = Offset.zero;
         const curve = Curves.easeInOutQuad;
         var tween =
-        Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
         var offsetAnimation = animation.drive(tween);
         return SlideTransition(position: offsetAnimation, child: child);
       },
